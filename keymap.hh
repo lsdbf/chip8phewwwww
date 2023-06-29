a7cc5ab95e98cb13e81fe4d7229c5388f8f1fb71
pbble <zdspeg@gmail.com>
2023-06-28 23:18:21 -0400

    ah

---
+++ keymap.hh
@@ -0,0 +1,26 @@
+#ifndef CHIP8PHEWWWWW_KEYMAP_H
+#define CHIP8PHEWWWWW_KEYMAP_H
+
+#endif //CHIP8PHEWWWWW_KEYMAP_H
+
+#include "chip.hh"
+#include <SDL2/SDL.h>
+
+const static uint8_t KEYMAP[FONT_SIZE/5] = {
+        SDLK_x, // 0
+        SDLK_1, // 1
+        SDLK_2, // 2
+        SDLK_3, // 3
+        SDLK_q, // 4
+        SDLK_w, // 5
+        SDLK_e, // 6
+        SDLK_a, // 7
+        SDLK_s, // 8
+        SDLK_d, // 9
+        SDLK_z, // A
+        SDLK_c, // B
+        SDLK_4, // C
+        SDLK_r, // D
+        SDLK_f, // E
+        SDLK_v  // F
+};
